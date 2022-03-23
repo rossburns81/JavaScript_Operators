86fea08dda1cbe167cd12702e4d0ce28c23af043
Ross Burns <101517539+rossburns81@users.noreply.github.com>
2022-03-23 12:35:31 -0500

    Completed

--- app.js
+++ app.js
@@ -7,13 +7,51 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+let exp = a ** b;
+let mod = a % b;
+
+console.log(`Add: ${add}, Minus: ${minus}, Multiply: ${multiply}, Dividing: ${dividing}, Exp: ${exp}, Mod: ${mod}`);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let c = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+console.log(c + str);
+console.log(c + str2);
+console.log(c + isPresent);
+console.log(c + firstName);
+console.log(isPresent + lastName);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(d == str3);
+console.log(d === str3);
+console.log(!isPresent2);
+console.log("eleven" == str4 && d >= str3);
+console.log(!isPresent2 || isPresent2);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !==false);
